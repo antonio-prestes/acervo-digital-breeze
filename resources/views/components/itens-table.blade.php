44be7182ca23b89a22bf75a40090a77799f42d92
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-10-09 21:22:22 -0300

    - Create and lis itens.

--- resources/views/components/itens-table.blade.php
+++ resources/views/components/itens-table.blade.php
@@ -1,7 +1,7 @@
 <table
-    class='mx-auto max-w-4xl w-full whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden mt-5 shadow'>
+    class='max-w-4xl w-full whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden mt-5 shadow'>
     <thead class="bg-gray-100 shadow">
-    <tr class="text-black text-left" style="height: 45px">
+    <tr class="text-black text-center" style="height: 45px">
         <th class="text-base font-medium text-gray-500 hover:text-gray-900">Titulo</th>
         <th class="text-base font-medium text-gray-500 hover:text-gray-900">Autor</th>
         <th class="text-base font-medium text-gray-500 hover:text-gray-900">Genero</th>
@@ -11,7 +11,7 @@
     </thead>
     <tbody class="divide-y divide-gray-100">
     @forelse($itens as $item)
-        <tr class="text-center">
+        <tr class="text-center" style="height: 22px">
             <td>{{$item->title}}</td>
             <td>{{$item->author}}</td>
             <td>{{$item->category->name}}</td>
