21c83c1df8a81b7a38a6e76b6c886d494c85f3e3
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-11-02 20:47:24 -0300

    - Ajuste e-mails teste unitários.

---
+++ tests/Unit/ContactControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\ContactController;
+use App\Http\Requests\ContactRequest;
+use App\Mail\ContactMail;
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+
+class ContactControllerTest extends TestCase
+{
+
+    public function testAccessContactPage()
+    {
+        $response = $this->get('/contact');
+
+        $response->assertStatus(200);
+    }
+
+    public function testSendContactEmail(): void
+    {
+        Mail::fake();
+
+        $email = new ContactRequest([
+            'name' => 'Diego',
+            'email' => 'diego@diego.com',
+            'message' => 'test'
+        ]);
+
+        $contactController = new ContactController();
+        $contactController->create($email);
+
+        Mail::assertSent(ContactMail::class);
+    }
+}
