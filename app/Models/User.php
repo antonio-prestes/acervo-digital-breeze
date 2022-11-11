9acb30323731c2ba1348a81a5d8e8acf85f3bc2b
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-11-11 00:00:37 -0300

    - Criado permissão de admin e user. - Tela de criação de usuário.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdministrator() {
+        return User::where('profile', 'admin')->exists();
+    }
 }
